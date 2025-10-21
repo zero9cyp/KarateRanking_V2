e28a11bc80981bf7c910fcf014b8ce8583c361a2
zero9cyp <svsnik71@gmail.com>
2025-10-22 01:05:33 +0300

    fix fron page

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,14 @@ router.get('/', ensureAuthenticated, (req, res) => {
   res.render('index', { userRole, currentUser, message: null });
 });
 
+// routes/authRoutes.js (example)
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) return res.redirect('/'); // fallback in case of error
+    res.clearCookie('connect.sid'); // optional, clears session cookie
+    res.redirect('/'); // <-- make sure this is a valid route, like your dashboard
+  });
+});
+
+
 module.exports = router;
